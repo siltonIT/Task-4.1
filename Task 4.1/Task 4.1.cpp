d36ff7128738a4d26e839656efb70ed0b4a0cd82
siltonIT <140279319+siltonIT@users.noreply.github.com>
2023-11-23 12:11:41 +0300

    Add project files.

---
+++ Task 4.1/Task 4.1.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <string.h>
+
+using std::endl;
+using std::cout;
+using std::cin;
+
+
+int main()
+{
+    const int SIZE = 5;
+    char text[SIZE];
+    char edit_text[SIZE];
+
+    while (cin.getline(text, SIZE))
+    {
+        if (!strcmp(text, ""))
+        {
+            break;
+        }
+    }
+
+    int checker = false;
+    int index = 0;
+
+    for (int i = 0; i < SIZE; ++i)
+    {
+        for (int n = 0; n < SIZE; ++n)
+        {
+            if (i == n)
+            {
+                continue;
+            }
+
+            if (text[i] != text[n])
+            {
+                checker = false;
+            }
+            else
+            {
+                checker = true;
+                break;
+            }
+        }
+
+        if (checker)
+        {
+            edit_text[index] = text[i];
+            ++index;
+        }
+    }
+
+    for (int i = 0; i < index; ++i)
+    {
+        cout << edit_text[i];
+    }
+
+}
